975ca8e28a1746a0d7157304dd117e1b48b927f4
Klinggaard <oliver.klinggaard@gmail.com>
2019-09-28 15:21:09 +0200

    Started gameState class

--- pygammon/game.py
+++ pygammon/game.py
@@ -9,3 +9,18 @@ class GameState:
         else:
             # Setup game
             self.state = np.empty((2, 15), dtype=np.int)  # 2 players, 15 tokens per player
+        if not empty:
+            self.state.fill(-1)
+
+    def copy(self):
+        return GameState(self.state.copy())
+
+    def __getitem__(self, item):
+        return self.state[item]
+
+    def __setitem__(self, key, value):
+        self.state[key] = value
+
+    def __iter__(self):
+        return self.state.__iter__()
+
